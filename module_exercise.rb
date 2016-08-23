f24ed35824b58dc720df7e7f7fb4eddd904666fb
Nina Mutty <ninamutty@gmail.com>
2016-08-23 10:24:29 -0700

    Class demo code

--- module_exercise.rb
+++ module_exercise.rb
@@ -10,8 +10,8 @@
 ######    EXAMPLE MODULE      #####
 ###################################
 
-  #
-  # module Perimeter
+
+  # module Perimeter #Same CamelCase naming convetions as will class - MODULES
   #   class Array
   #     def initialize
   #       @size = 400
@@ -22,10 +22,12 @@
   # our_array = Perimeter::Array.new
   # ruby_array = Array.new
   #
-  # p our_array.class
-  # p ruby_array.class
+  # puts our_array.class
+  # puts ruby_array.class
   #
+  # #This will give an error message because we haven't defined length in the Perimeter::Array class
   # p our_array.length
+
 
 
 
@@ -54,16 +56,17 @@
 #DOCUMENTATION:
 #https://github.com/kqdreger/traveller
 
-#Testing out the traveller gem!
+# Testing out the traveller gem!
 
-  # require 'traveller'
-  #
-  # trav = Traveller.new("Seattle, Minnesota 98101")
+  require 'traveller'
+
+  # traveller1 = Traveller.new("Seattle, Minnesota 98101")
+
 
   # puts traveller1.city
   # puts traveller1.zip
   #
-  # trav.state = "Washington"
+  # traveller1.state = "Washington"
   #
   # puts traveller1.state
 
@@ -73,22 +76,28 @@
 ###################################
 
 #I want to create a new class for travellers, called traveller. What could possibly go wrong?
-  # class Traveller
-  #   attr_accessor :name, :email, :city, :state
-  #   def initialize(details_hash)
-  #     @name = details_hash[:name]
-  #     @email = details_hash[:email]
-  #     @location = Traveller.new(details_hash[:location])
-  #   end
-  # end
-
-  #Tests to use the Traveller Gem (after my class has been created. )
-    # traveller2 = Traveller.new("Chicago 60611 IL")
-    # puts Traveller.state
-
-  #Hmm. It doesn't work. Oh bother.
-  #How would we possibly fix this so I don't have to change the name of MY class but still be able to use this gem?
-
+# module Expedia
+#   class Traveller
+#     attr_accessor :name, :email, :location
+#     def initialize(details_hash)
+#       @name = details_hash[:name]
+#       @email = details_hash[:email]
+#       @location = details_hash[:location]
+#     end
+#   end
+# end
+#
+#   #Tests to use the Traveller Gem (after my class has been created. )
+#     traveller2 = Traveller.new("Chicago 60611 IL")
+#     puts traveller2.state
+#
+#   #Hmm. It doesn't work. Oh bother.
+#   #How would we possibly fix this so I don't have to change the name of MY class but still be able to use this gem?
+#
+#
+#
+#   user1 = Expedia::Traveller.new(name: "name", email: "email", location: Traveller.new("Seattle, Washington 98102"))
+#
 
 
   ###################################
@@ -104,44 +113,45 @@
   # Outside the class or module, they may be accessed using the scope operator, :: prefixed by an expression that returns the appropriate class or module.
 
   # Constants defined outside any class or module may be accessed as it is or by using the scope operator with no prefix.
+
   # Constants may not be defined in methods.
 
   # Constants may be added to existing classes and modules from the outside by using the class or module name and the scope operator before the constant name.
 
 
 
-      #   module Blackjack
-      #     MAX_SCORE = 21
-      #     module Player
-      #         MAX_PLAYERS = 2
-      #         class Player
-      #           def initialize
-      #           end
-      #         end
-      #     end
-      #     module Card
-      #         MAX_VALUE = 13
-      #         class Card
-      #           def get_max_value()
-      #               return MAX_VALUE
-      #           end
-      #         end
-      #     end
-      # end
-      #
-      # MAX_SCORE = 50
-      # MAX_PLAYERS = 4
-      # MAX_VALUE = 21
-      #
-      # puts "MAX_SCORE - #{MAX_SCORE}"
-      # puts "Blackjack::MAX_SCORE - #{Blackjack::MAX_SCORE}"
-      #
-      # puts
-      #
-      # puts "MAX_Players - #{MAX_PLAYERS}"
-      # puts "Blackjack::Player::MAX_Players - #{Blackjack::Player::MAX_PLAYERS}"
-      #
-      # puts
-      #
-      # puts "MAX_VALUE - #{MAX_VALUE}"
-      # puts "Blackjack::Card::get_max_value -  #{Blackjack::Card::Card.new().get_max_value()}"`
+    module Blackjack
+        MAX_SCORE = 21
+        module Player
+            MAX_PLAYERS = 2
+            class Player
+                def initialize
+                end
+            end
+        end
+        module Card
+            MAX_VALUE = 13
+            class Card
+                def get_max_value()
+                    return MAX_VALUE
+                end
+            end
+        end
+    end
+
+    MAX_SCORE = 50
+    MAX_PLAYERS = 4
+    MAX_VALUE = 21
+
+    puts "MAX_SCORE - #{MAX_SCORE}"
+    puts "Blackjack::MAX_SCORE - #{Blackjack::MAX_SCORE}"
+
+    puts
+
+    puts "MAX_Players - #{MAX_PLAYERS}"
+    puts "Blackjack::Player::MAX_Players - #{Blackjack::Player::MAX_PLAYERS}"
+
+    puts
+
+    puts "MAX_VALUE - #{MAX_VALUE}"
+    puts "Blackjack::Card::get_max_value -  #{Blackjack::Card::Card.new().get_max_value()}"
